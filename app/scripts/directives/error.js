8ee341264a82cc3466d96221415adaddb7a1e1aa
roland.gude <roland.gude@localhost>
2014-11-18 10:50:10 +0100

    RSYS-1561
    correct addresslines for invoice

---
+++ app/scripts/directives/error.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name ycBookingApp.directive:error
+ * @description
+ * # error
+ */
+angular.module('ycBookingApp')
+    .directive('error', function () {
+        return {
+            template: '<div class="col-sm-12" ng-repeat="error in cause"><div class="alert alert-danger" translate="{{error}}" translate-values="{{variables}}"></div></div>',
+            restrict: 'E',
+            scope: {
+                cause: '=',
+                variables: '='
+            },
+
+            controller: function ($scope, $stateParams) {
+                
+                if (!$scope.cause instanceof Array) {
+                    $scope.cause = [$scope.cause];
+                }
+                for (var i = 0; i < $scope.cause.length; i++){
+                    if ($scope.cause[i] == "") {
+                        $scope.cause[i] = "UnmappedError";
+                    }
+                }
+            }
+        };
+    });
