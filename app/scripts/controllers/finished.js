88239e5955537ab0336730cfb1b341ab343bc904
johannes.hoerle <johannes.hoerle@localhost>
2015-01-16 15:41:58 +0100

    RSYS-985 fixed ?!?!? payPal email while booking

---
+++ app/scripts/controllers/finished.js
@@ -0,0 +1,40 @@
+/**
+ * @ngdoc function
+ * @name ycBookingApp.controller:FinishedCtrl
+ * @description
+ * # FinishedCtrl
+ * Controller of the ycBookingApp
+ */
+angular.module('ycBookingApp')
+    .controller('FinishedCtrl', function ($scope, $stateParams, $timeout, ycRestfrontend) {
+        'use strict';
+
+        $scope.contractid = $stateParams.contractid;
+        $scope.customerid = $stateParams.customerid;
+        $scope.orderid = $stateParams.orderid;
+
+        $scope.setupFinished = false;
+        $scope.error = false;
+
+        ycRestfrontend.confirmOrder({
+            orderId: $stateParams.orderid,
+            contractId: $stateParams.contractid
+        }).$promise.then(function (response) {
+            $scope.setupFinished = true;
+            $scope.error = false;
+            $scope.mandatorid = response.createdMandators;
+        }, function(error){
+        	console.log(error);
+            $scope.errorVariables = {
+                'orderid': $scope.orderid,
+                'customerid': $scope.customerid,
+                'contractid': $scope.contractid
+            };
+
+        	$scope.setupFinished = true;
+        	$scope.errorCode = ['order_confirmation_error'];
+        	$scope.error = true;
+        });
+
+
+    });
