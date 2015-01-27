7a3b67b4820e5f2b1f27cce18ec479684f4704c5
roland.gude <roland.gude@localhost>
2015-01-27 11:44:15 +0100

    RSYS-1722
    tracking with piwik

---
+++ app/scripts/controllers/finished.js
@@ -0,0 +1,58 @@
+/**
+ * @ngdoc function
+ * @name ycBookingApp.controller:FinishedCtrl
+ * @description
+ * # FinishedCtrl
+ * Controller of the ycBookingApp
+ */
+angular.module('ycBookingApp')
+    .controller('FinishedCtrl', function ($scope, $stateParams, $timeout, ycRestfrontend) {
+        'use strict';
+
+        $scope.contractid = $stateParams.contractid;
+        $scope.customerid = $stateParams.customerid;
+        $scope.orderid = $stateParams.orderid;
+
+        $scope.setupFinished = false;
+        $scope.error = false;
+
+        ycRestfrontend.confirmOrder({
+            orderId: $stateParams.orderid,
+            contractId: $stateParams.contractid
+        }).$promise.then(function (response) {
+            if (window._paq){
+                window._paq.push(['trackEvent',
+                    'yc-contract-confirmed',
+                    $scope.contractid
+                ]);
+            };
+            if (window._paq){
+                window._paq.push(['trackEvent',
+                    'yc-mandator-created',
+                    response.createdMandators
+                ]);
+            };
+            $scope.setupFinished = true;
+            $scope.error = false;
+            $scope.mandatorid = response.createdMandators;
+        }, function(error){
+        	console.log(error);
+            $scope.errorVariables = {
+                'orderid': $scope.orderid,
+                'customerid': $scope.customerid,
+                'contractid': $scope.contractid
+            };
+            if (window._paq){
+                window._paq.push(['trackEvent',
+                    'yc-contract-confirmation-error',
+                    "contract:"+ $scope.contractid + ', order:' + $scope.orderid + ', customer:' + $scope.customerid
+                ]);
+            };
+
+        	$scope.setupFinished = true;
+        	$scope.errorCode = ['order_confirmation_error'];
+        	$scope.error = true;
+        });
+
+
+    });
---
+++ app/scripts/directives/error.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name ycBookingApp.directive:error
+ * @description
+ * # error
+ */
+angular.module('ycBookingApp')
+    .directive('error', function () {
+        return {
+            template: '<div class="col-sm-12" ><div class="alert alert-danger"><ul style="margin-bottom: 15px;"><li ng-repeat="error in cause" translate="{{error}}" translate-values="{{variables}}" ></li></ul><div ng-if="message" class="panel panel-warning"><div class="panel-heading">Message</div><div class="panel-body">{{message}}</div></div><div ng-if="details" class="panel panel-warning"><div class="panel-heading">Details</div><div class="panel-body">{{details}}</div></div></div></div></div>',
+            restrict: 'E',
+            scope: {
+                cause: '=',
+                variables: '=',
+                message: '=',
+                details: '='
+            },
+
+            controller: function ($scope, $stateParams) {
+                
+                if (!$scope.cause instanceof Array) {
+                    $scope.cause = [$scope.cause];
+                }
+                for (var i = 0; i < $scope.cause.length; i++){
+                    if ($scope.cause[i] == "") {
+                        $scope.cause[i] = "UnmappedError";
+                    }
+                }
+            }
+        };
+    });
