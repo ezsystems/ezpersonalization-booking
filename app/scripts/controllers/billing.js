3cbbb5eb0fa5e99b4432a4b278605b789cf3ffa4
roland.gude <roland.gude@localhost>
2015-04-20 14:18:42 +0200

    hg flow, merge release `3.2.1100` to publish branch `default`

---
+++ app/scripts/controllers/billing.js
@@ -0,0 +1,34 @@
+/**
+ * @ngdoc function
+ * @name ycBookingApp.controller:BillingCtrl
+ * @description
+ * # BillingCtrl
+ * Controller of the ycBookingApp
+ */
+angular.module('ycBookingApp')
+    .controller('BillingCtrl', function ($scope, tab, $sessionStorage, location) {
+        'use strict';
+        $scope.countries = tab.countries;
+        if (!$scope.billing.country){
+            $scope.billing.country = location;
+        }
+
+        if (!$scope.billing.email) {
+            $scope.billing.email = $scope.account.email;
+        }
+        if (!$scope.billing.firstname) {
+            $scope.billing.firstname = $scope.account.firstname;
+        }
+        if (!$scope.billing.lastname) {
+            $scope.billing.lastname = $scope.account.lastname;
+        }
+    
+        $scope.storeSession = function () {
+            $sessionStorage.billing = $scope.billing;
+        };
+
+        $scope.$on('$stateChangeStart', 
+	        function(event, toState, toParams, fromState, fromParams){
+	            $scope.storeSession();
+	        })
+    });
